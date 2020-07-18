23e851015ed874aaadfa92fa7b9ecb5192273168
tcarns <68445375+tcarns@users.noreply.github.com>
2020-07-18 19:19:20 -0400

    initial commit; style for various buttons added

---
+++ tippy.js
@@ -0,0 +1,27 @@
+tippy('#biggerMugs', {
+  allowHTML: true,
+  content: '<center>Bigger mugs means more brew slung. More brew slung means more gold!</center>',
+  placement: 'auto-end',
+  maxWidth: 250,
+});
+
+tippy('#bustierMaids', {
+  allowHTML: true,
+  content: '<center>Particularly... <i>excited</i> customers tend to spend more gold if our maids have certain... <i>assets.</i></center>',
+  placement: 'auto-end',
+  maxWidth: 250,
+});
+
+tippy('#barkeepers', {
+  allowHTML: true,
+  content: '<center>They may not be the most dashing barkeeps, but they get the job done.</center>',
+  placement: 'auto-end',
+  maxWidth: 250,
+});
+
+tippy('#moreMaids', {
+  allowHTML: true,
+  content: '<center>These lovely lasses tend to motivate customers to spend more money on your legendary brew.</center>',
+  placement: 'auto-end',
+  maxWidth: 250,
+});
