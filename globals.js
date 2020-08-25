80776eb695ac85325b543b5c3358ab8c906ccee6
tcarns <officialtylergcarns@yahoo.com>
2020-08-24 20:29:22 -0400

    gold total changed back to zero

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
-var gold                      = 100000.0
-var trueGold                  = 100000.0 // 'gold' is truncated in the game loop whereas 'trueGold' isn't.
+var gold                      = 0.0
+var trueGold                  = 0.0 // 'gold' is truncated in the game loop whereas 'trueGold' isn't.
 var passiveIncome             = 0.0
 var clickIncome               = 1.0
 var version                   = 0.01
