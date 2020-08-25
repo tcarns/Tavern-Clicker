9e3316a0b79865292526c3bf36c397b4c22c8746
tcarns <officialtylergcarns@yahoo.com>
2020-08-24 20:19:48 -0400

    initial commit; holds global variables

---
+++ globals.js
@@ -0,0 +1,5 @@
+var gold                      = 100000.0
+var trueGold                  = 100000.0 // 'gold' is truncated in the game loop whereas 'trueGold' isn't.
+var passiveIncome             = 0.0
+var clickIncome               = 1.0
+var version                   = 0.01
