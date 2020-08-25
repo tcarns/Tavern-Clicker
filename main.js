4a2a0614c63b6115151d1489c263b196889ec558
tcarns <officialtylergcarns@yahoo.com>
2020-08-24 20:22:15 -0400

    version control: rebuilds game data when loading from an older version

--- main.js
+++ main.js
@@ -1,43 +1,8 @@
 // Holds information for loading and saving.
 var saveState = {}
 
-// Holds information pertaining to the amount of every built building. See: updateBuildings().
+// Holds information pertaining to the amount of every built building. See: main.updateBuildings().
 var buildList = []
-
-// Tracks all purchased upgrades.
-var upgradesPurchased = []
-
-var gold = 0.0
-var trueGold = 0.0 // 'gold' is truncated in the game loop whereas 'trueGold' isn't.
-var passiveIncome = 0.0
-var clickIncome = 1.0
-
-// Dictionary containing all the necessary attributes for buildings. Structure: [0] = updateString (for properly updating HTML), [1] = building description,
-// [2] = cost, [3] = clickChange (change in click income), [4] = passiveChange (change in passive income), [5] = htmlID (for lookup by ID from the HTML document),
-// [6] = totalPurchased (amount that building's been purchased), [7] = buildListS (a string defining how one building should be shown in the HTML building list),
-// [8] = buildListP (a string defining how multiple of that building should be shown in the HTML building list).
-var buildings = [
-  { updateString: 'More Maids: ', buildingDescription: 'Increases passive income by 1', cost: 10.0, clickChange: 0.0, passiveChange: 1.0, htmlID: 'moreMaids',
-  totalPurchased: 0, buildListS: ' maid tending to customers', buildListP: ' maids tending to customers' }, // moreMaids, id = 0.
-  { updateString: 'Barkeepers: ', buildingDescription: 'Autoclicks every 0.5 seconds', cost: 10.0, clickChange: 0.0, passiveChange: (clickIncome/2.0),
-  htmlID: 'barkeepers', totalPurchased: 0, buildListS: ' barkeeper slinging brew', buildListP: ' barkeepers slinging brew'} // barkeepers, id = 1.
-]
-
-// Dictionary containing all the necessary attributes for buildings. Structure: [0] = cost, [1] = clickChange (change in click income),
-// [2] = passiveChange (change in passive income), [3] = htmlID (for lookup by ID from the HTML document)
-// [4] = relation (defines a function if a particular upgrade has an impact on something else).
-var upgrades = [
-  { cost: 15.0, clickChange: 1.0, passiveChange: 0.0, htmlID: 'biggerMugs', relation: function() {
-      passiveIncome -= (buildings[1].totalPurchased * buildings[1].passiveChange)
-      buildings[1].passiveChange *= 2
-      passiveIncome += (buildings[1].totalPurchased * buildings[1].passiveChange)
-  }}, // biggerMugs, id = 0.
-  { cost: 50.0, clickChange: 0.0, passiveChange: 0.0, htmlID: 'bustierMaids', relation: function() {
-      passiveIncome -= (buildings[0].totalPurchased * buildings[0].passiveChange)
-      buildings[0].passiveChange *= 1.25
-      passiveIncome += (buildings[0].totalPurchased * buildings[0].passiveChange)
-  }} // bustierMaids, id = 1.
-]
 
 // Defines click income: updates the player's gold total and updates HTML.
 function updateGoldClick(id)  {
@@ -47,48 +12,51 @@ function updateGoldClick(id)  {
 
 // Purchases an upgrade: subtracts its cost from the player's gold total, updates click/passive income, updates HTML.
 function purchaseUpgrade(id) {
-  if(trueGold < upgrades[id].cost) return
+  if(trueGold < upgrades[id].cost) {
+    return
+  }
   trueGold -= upgrades[id].cost
   clickIncome += upgrades[id].clickChange
   passiveIncome += upgrades[id].passiveChange
   upgradesPurchased[id] = true
-  document.getElementById(upgrades[id].htmlID).remove()
-  upgrades[id].relation()
+  if(document.getElementById(upgrades[id].htmlID) != null) {
+    document.getElementById(upgrades[id].htmlID).remove()
+  }
+  upgrades[id].effect()
 }
 
-// Purchases a building: subtracts its cost from the player's gold total, updates click/passive income, updates the building's cost, updates HTML,
-// increments the amount of times the building has been purchased.
+// Purchases a building: subtracts its cost from the player's gold total, updates click/passive income, updates HTML.
 function purchaseBuilding(id) {
-  if(trueGold < buildings[id].cost) {
+  var buildingCost = buildings[id].baseCost * Math.pow(1.15, buildings[id].totalPurchased)
+  if(trueGold < buildingCost) {
     return
   }
-  trueGold -= buildings[id].cost
+  trueGold -= buildingCost
   clickIncome += buildings[id].clickChange
   passiveIncome += buildings[id].passiveChange
-  buildings[id].cost = Math.trunc(buildings[id].cost * 1.2)
-  document.getElementById(buildings[id].htmlID).innerHTML = buildings[id].updateString + (buildings[id].cost) + ' gold<br>' + buildings[id].buildingDescription
+  buildings[id].totalPurchased++
+  document.getElementById(buildings[id].htmlID).innerHTML = buildings[id].updateString + (buildingCost) + ' gold<br>' + buildings[id].description
   updateBuildings(id)
 }
 
-// Updates the list of buildings that have been purchased: increments the total amount of that building purchased, handles singular/plural, builds the
-// building list, excluding null values, then updates HTML.
+// Updates the list of buildings that have been purchased: handles singular/plural, builds the building list, excluding null values, then updates HTML.
 function updateBuildings(id) {
-  buildings[id].totalPurchased++
   if(buildings[id].totalPurchased > 1) {
     buildList[id] = buildings[id].totalPurchased + buildings[id].buildListP + '<br>'
-  } else {
+  } else if (buildings[id].totalPurchased == 1) {
     buildList[id] = buildings[id].totalPurchased + buildings[id].buildListS + '<br>'
   }
   updateBuildingsHTML()
 }
 
-// Updates HTML of purchased buildings on game load.
+// Updates HTML of purchased buildings when a building is purchased or on game load.
 function updateBuildingsHTML() {
   buildStr = ''
   for(var i = 0; i < buildList.length; i++) {
     if(buildList[i] != null) {
       buildStr += buildList[i]
-      document.getElementById(buildings[i].htmlID).innerHTML = buildings[i].updateString + (buildings[i].cost) + ' gold<br>' + buildings[i].buildingDescription
+      var newHTML = buildings[i].updateString + Math.trunc(buildings[i].baseCost * Math.pow(1.15, buildings[i].totalPurchased)) + ' gold<br>' + buildings[i].description
+      document.getElementById(buildings[i].htmlID).innerHTML = newHTML
     }
   }
   document.getElementById('buildingList').innerHTML = buildStr
@@ -97,8 +65,33 @@ function updateBuildingsHTML() {
 // Updates HTML of purchased upgrades on game load.
 function updateUpgradesHTML() {
   for(var i = 0; i < upgradesPurchased.length; i++) {
-    if(upgradesPurchased[i] == true) {
+    if(upgradesPurchased[i] == true && document.getElementById(upgrades[i].htmlID) != null) {
       document.getElementById(upgrades[i].htmlID).remove()
+    }
+  }
+}
+
+// Rebuilds building data when a new version is detected on game load.
+function rebuildBuildingData() {
+  for(var i = 0; i < buildings.length; i++) {
+    var totalPurchased = saveState.buildings[i].totalPurchased
+    clickIncome += (buildings[i].clickChange * totalPurchased)
+    passiveIncome += (buildings[i].passiveChange * totalPurchased)
+    buildings[i].totalPurchased = totalPurchased
+    updateBuildings(i)
+  }
+}
+
+// Rebuilds upgrade data when a new version is detected on game load.
+function rebuildUpgradeData() {
+  for(var i = 0; i < upgrades.length; i++) {
+    if(saveState.upgradesPurchased[i]) {
+      clickIncome += upgrades[i].clickChange
+      passiveIncome += upgrades[i].passiveChange
+      upgrades[i].effect()
+      if(document.getElementById(upgrades[i].htmlID) != null) {
+        document.getElementById(upgrades[i].htmlID).remove()
+      }
     }
   }
 }
@@ -112,27 +105,38 @@ function saveGame() {
   saveState.buildList = buildList
   saveState.buildings = buildings
   saveState.upgradesPurchased = upgradesPurchased
+  saveState.upgrades = upgrades
+  saveState.version = version
   localStorage.setItem('save', JSON.stringify(saveState))
   console.log('Game saved.')
 }
 
-// Loads the game.
+// Loads the game. Checks for a new player (if no saveState is present) or if a new version is present.
 function loadGame() {
     saveState = JSON.parse(localStorage.getItem('save'))
-    // If no save is present (i.e. it's the first time the game is loaded), load nothing.
-    if(saveState == null) {
+    if(saveState == null) { // If no save is present (i.e. it's the first time the game is loaded), load nothing.
       document.getElementById('mainBody').style.display = 'block'
       return
     }
-    gold = saveState.gold
-    trueGold = saveState.trueGold
-    passiveIncome = saveState.passiveIncome
-    clickIncome = saveState.clickIncome
-    buildList = saveState.buildList
-    buildings = saveState.buildings
-    upgradesPurchased = saveState.upgradesPurchased
-    updateBuildingsHTML()
-    updateUpgradesHTML()
+    if(version != saveState.version) {
+      console.log('New version detected.')
+      rebuildBuildingData()
+      rebuildUpgradeData()
+      updateUpgradesHTML()
+      upgradesPurchased = saveState.upgradesPurchased
+      gold = saveState.gold
+      trueGold = saveState.trueGold
+    } else {
+      gold = saveState.gold
+      trueGold = saveState.trueGold
+      passiveIncome = saveState.passiveIncome
+      clickIncome = saveState.clickIncome
+      buildList = saveState.buildList
+      buildings = saveState.buildings
+      upgradesPurchased = saveState.upgradesPurchased
+      updateBuildingsHTML()
+      updateUpgradesHTML()
+    }
     if(buildList.length == 0) {
       document.getElementById('buildingList').innerHTML = 'No buildings or employees currently purchased.'
     }
